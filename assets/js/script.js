70a23012ce81e42a6530e13e2dbd1ce286edceb7
JessPorto <jessica.portomig@gmail.com>
2022-07-06 20:08:47 +0000

    Add pickRandom function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,47 @@
 /**
- * Creates a array of numbers from 1 and 10
+ * Creates one number between 1 and 10
  */
  function numberRange() {
   return Math.round(Math.random() * 10);
 
 }
 
+/**
+* Pick one random number
+*/
+function pickRandom(quantity) {
+
+  let secret = [];
+  let number = 1;
+
+  while (number <= quantity) {
+
+    let randomNumber = numberRange();
+    if (randomNumber !== 0) {
+      let found = false;
+
+      for (var position = 0; position < secret.length; position++) {
+
+        if (secret[position] == randomNumber) {
+
+          found = true;
+          break;
+        }
+      }
+
+      if (found == false) {
+        secret.push(randomNumber);
+        number++;
+      }
+
+    }
+
+  }
+  return secret;
+}
+
+let secret = pickRandom(3);
+
+let input = document.querySelector(".form-control");
+
+input.focus();
